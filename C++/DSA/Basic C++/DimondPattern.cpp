ee82f476ad80209a02772d04c90977d445909455
Md. Shihab Sarar <shihabsarar582003@gmail.com>
2025-04-16 00:55:20 +0600

    Practice

---
+++ C++/DSA/Basic C++/DimondPattern.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "n = ";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n-i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= 2*i-1; j++)
+        {
+            cout << "* ";
+            /*if we use  j<=i then 
+            this condition is required.*/
+
+            // if (j!=1)
+            // {
+            //     cout << "* ";
+            // }
+            
+        }
+        cout << endl;
+    }
+
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= n-i; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= 2*i-1; j++)
+        {
+            cout << "* ";
+            /*if we use  j<=i then 
+            this condition is required.*/
+
+            // if (j!=1)
+            // {
+            //     cout << "* ";
+            // }
+            
+        }
+        cout << endl;
+    }
+    
+}
--- C++/DSA/Basic C++/RhombusPattern.cpp
+++ C++/DSA/Basic C++/RhombusPattern.cpp
@@ -5,9 +5,15 @@ int main()
     int n;
     cout << "n = ";
     cin >> n;
-    for (int i = n; i >= 1; i--)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = i; j >= 1; j--)
+        for (int j = n-i; j >= 1; j--)
+        {
+            cout << "  ";
+        }
+        
+
+        for (int j = 1; j <= n; j++)
         {
             cout << "* ";
         }
