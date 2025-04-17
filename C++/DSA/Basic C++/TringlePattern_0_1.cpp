ea7aa682b1bd59b7a826d31ce5ae65f1bc77398b
Md. Shihab Sarar <shihabsarar582003@gmail.com>
2025-04-17 13:24:18 +0600

    Problem Solve

--- C++/DSA/Basic C++/TringlePattern_0_1.cpp
+++ C++/DSA/Basic C++/TringlePattern_0_1.cpp
@@ -1,15 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int n = 5;
+    int n;
+    cout << "Enter Row Number: ";
+    cin >> n;
+    bool val = 1;
     for (int i = 1; i <= n; i++)
     {
+
         for (int j = 1; j <= i; j++)
         {
-            
+            cout << val;
+            val = !val;
+            // if (i % 2 != 0)
+            // {
+            //     if (j == 1)
+            //     {
+            //         cout << 1;
+            //     }
+            //     else
+            //     {
+            //         cout << "01";
+            //         j++;
+            //     }
+            // }
+            // else
+            // {
+            //     cout << "01";
+            //     j++;
+            // }
         }
         cout << endl;
     }
-    
 }
