bd1077b560e99f0c2d839127546abc86a17532bd
Md. Shihab Sarar <shihabsarar582003@gmail.com>
2025-05-06 01:23:27 +0600

    modification

--- C++/DSA/BubbleSort/BubbleSort.cpp
+++ C++/DSA/BubbleSort/BubbleSort.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void printArray(int a[], int n){
+void printArray(int a[], int n)
+{
     cout << "Sorted Array: ";
     for (int i = 0; i < n; i++)
     {
@@ -10,26 +11,35 @@ void printArray(int a[], int n){
     cout << endl;
 }
 
-void bubbleSort(int a[], int n){
-    for (int i = 0; i < n-1; i++)
+void bubbleSort(int a[], int n)
+{
+    bool isSwap = false;
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n-i-1; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if (a[j]>a[j+1])
+            if (a[j] > a[j + 1])
             {
                 int temp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+                isSwap = true;
             }
-            
+        }
+        if (!isSwap)
+        {
+            cout << "The array is already sorted." << endl;
+            return;
         }
         
     }
     printArray(a, n);
 }
-int main(){
-    int a[]={78, 55, 34, 90, 64, 37, 59, 85};
-    int n = sizeof(a)/sizeof(int);
+int main()
+{
+    // int a[] = {78, 55, 34, 90, 64, 37, 59, 85};
+    int a[] = {1, 2, 3, 4, 5};
+    int n = sizeof(a) / sizeof(int);
     bubbleSort(a, n);
     return 0;
 }
