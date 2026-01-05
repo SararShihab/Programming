2a2edd672e488f280c46e25d940285d5f8ef21e2
Md. Shihab Sarar <69312373+whitemafia@users.noreply.github.com>
2026-01-05 13:07:16 +0600

    .

---
+++ Python/draft.py
@@ -0,0 +1,4 @@
+Str1 = "hands-on machine learning mentorship progrAm with live classes by Shovo BhaIya"
+vow = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'}
+rslt = set(Str1) & vow
+print("No of unique vowels -",len(rslt))
