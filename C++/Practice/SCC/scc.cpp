7d5ac8d50fb21ee53ba5e23ac03ccc9f73368873
Md. Shihab Sarar <69312373+whitemafia@users.noreply.github.com>
2026-02-21 23:34:00 +0600

    scc

---
+++ C++/Practice/SCC/scc.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class SCC {
+private:
+
+    void dfs1(int node, vector<int>& vis, vector<vector<int>>& adj, stack<int>& st) {
+        vis[node] = 1;
+
+        for (int neighbor : adj[node]) {
+            if (!vis[neighbor]) {
+                dfs1(neighbor, vis, adj, st);
+            }
+        }
+
+        st.push(node);
+    }
+
+    void dfs2(int node, vector<int>& vis, vector<vector<int>>& adjT, vector<int>& component) {
+        vis[node] = 1;
+        component.push_back(node);
+
+        for (int neighbor : adjT[node]) {
+            if (!vis[neighbor]) {
+                dfs2(neighbor, vis, adjT, component);
+            }
+        }
+    }
+
+public:
+
+    void kosaraju(int V, vector<vector<int>>& adj) {
+
+        vector<int> vis(V, 0);
+        stack<int> st;
+
+        // Step 1: First DFS
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs1(i, vis, adj, st);
+            }
+        }
+
+        // Step 2: Create transpose graph
+        vector<vector<int>> adjT(V);
+
+        for (int i = 0; i < V; i++) {
+            vis[i] = 0;
+            for (int neighbor : adj[i]) {
+                adjT[neighbor].push_back(i);
+            }
+        }
+
+        // Step 3: Second DFS
+        int sccCount = 0;
+
+        while (!st.empty()) {
+            int node = st.top();
+            st.pop();
+
+            if (!vis[node]) {
+                vector<int> component;
+
+                dfs2(node, vis, adjT, component);
+
+                sccCount++;
+
+                cout << "SCC " << sccCount << ": ";
+                for (int n : component) {
+                    cout << n << " ";
+                }
+                cout << endl;
+            }
+        }
+
+        cout << "Total Strongly Connected Components: " << sccCount << endl;
+    }
+};
+
+int main() {
+
+    int V, E;
+
+    cout << "Enter number of vertices: ";
+    cin >> V;
+
+    cout << "Enter number of edges: ";
+    cin >> E;
+
+    vector<vector<int>> adj(V);
+
+    cout << "Enter directed edges (u v):\n";
+    for (int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+
+    SCC obj;
+    obj.kosaraju(V, adj);
+
+    return 0;
+}
